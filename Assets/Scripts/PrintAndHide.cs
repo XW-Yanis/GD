f4355326780e4da081183a9e1cb8f57191554661
Xiang WENG <xwen3544@uni.sydney.edu.au>
2023-08-30 17:19:16 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -4,16 +4,25 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     private int i;
+    int randomValue;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        randomValue = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
-        Debug.Log(gameObject.name+":"+i);   
+        Debug.Log(gameObject.name+":"+i); 
+        if (i == 100 && gameObject.tag.Equals("Red"))
+        {
+            gameObject.SetActive(false);
+        } else if (i == randomValue && gameObject.tag.Equals("Blue"))
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
     }
 }
